33c7cf4147af470be030d4ae9db52530a8fe391a
Madeline Follosco <mvfollosco@gmail.com>
2026-02-27 18:52:27 -0800

    define get user route

---
+++ backend/src/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+# firebase import
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("path/to/serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+# intialize database
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+app = Flask(__name__)
+# method to get users from firestore
+@app.route("/firebase/get" , methods=["GET"])
+def firebase_get():
+    db = firestore.client()
+    doc = db.collection("users").get()
+    if doc.exists:
+        return jsonify(doc.to_dict())
+    return jsonify({"error": "Document not found"}), 404
+
+if __name__ == "__main__":
+    app.run(port=8080,debug=True)
